7b4aa6a35ec9a5a1bdf8abe3a4ac8206c15598d1
Ta-Cuong Nguyen <cuong@planday.com>
2017-12-25 18:57:05 +0700

    Add tracking time

--- src/com/cuongnotes/Main.java
+++ src/com/cuongnotes/Main.java
@@ -9,15 +9,20 @@ public class Main {
 
     }
     public void devidedNumberToBinary(int number) {
+        long startTime = System.nanoTime();
         MyStack myStack = new MyStack(100);
         while (number > 0) {
             myStack.push(number % 2);
-            System.out.println("Push value to stack " + (number %2));
+            System.out.println("Push value to stack " + (number % 2));
             number = number / 2;
         }
         while (!myStack.isEmpty()) {
             System.out.print(myStack.pop());
         }
+        System.out.print("\n");
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Total time: " + duration);
     }
 
 }
